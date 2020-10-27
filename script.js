e37cba030b094cd94b037bcb22100d5c0c1099fc
Elliot Lemaire <lemaireelliot@wanadoo.fr>
2020-10-27 14:30:02 +0100

    premier essai Javascript

--- script.js
+++ script.js
@@ -1,5 +1,79 @@
-/* Function permettant d'afficher les articles disponibles */
+/* Constantes et variables globales */
+const itemsFetch = fetch('http://localhost:3000/api/cameras');
+var itemsStorage = document.getElementById('items-storage'); //Chemin vers la liste des articles de la Boutique.
 
+
+//Au chargement de la page Boutique.
+document.body.onload = function() {
+  //Initialisation de la requete fetch.
+  itemsFetch.then(function(response) {
+    return response.json();
+  }).then(function(json) {
+    let items = json;
+    initialize(items);
+  }).catch(function(err) {
+    itemsStorage.innerHTML = "Impossible d'afficher les articles";
+  });
+}
+
+//Fonction globale permettant d'afficher les articles disponibles.
+function initialize(items) {
+  itemsList = items; //Database globale des articles
+  updateItems();
+  itemsList = [];
+
+  //Fonction qui crée les chemins d'accès aux images des articles.
+  /*
+  function fetchBlob(item) {
+    let url = 'back-end/images/' + product.image;
+    fetch(url).then(function(response) {
+      return response.blob();
+    }).then(function(blob) {
+      let itemPath = URL.createObjectURL(blob);
+      updateItems(itemPath, item)
+    });
+  }*/
+
+  //Fonction permettant d'actualiser l'affichage des articles.
+  function updateItems(itemPath, item) {
+    //Création des éléments constituant un Item.
+    const addItem = document.createElement('div');
+    const imageHolder = document.createElement('div');
+    const addImage = document.createElement('img');
+    const descriptionHolder = document.createElement('div');
+    const addName = document.createElement('h3');
+    const addText = document.createElement('p');
+    const addLense = document.createElement('h4');
+    const addPrice = document.createElement('h4');
+
+    //Définition des classes pour chaque div composant un article.
+    addItem.setAttribute('class', 'row store-item');
+    imageHolder.setAttribute('class', 'col-12 col-lg-4');
+    descriptionHolder.setAttribute('class', 'col-12 col-lg-8');
+
+    //Ajout du contenu des titres et descriptions des articles.
+    addName.textContent = item.name;
+    addText.textContent = item.description;
+
+    //Création des attributs src et alt des images.
+    img.src = item.imageUrl;
+    img.alt = item.name;
+
+    //Répartition des différents éléments composant un article.
+    itemsStorage.appendChild(addItem);
+    addItem.appendChild(imageHolder);
+    addItem.appendChild(descriptionHolder);
+    imageHolder.appendChild(addImage);
+    descriptionHolder.appendChild(addName);
+    descriptionHolder.appendChild(addText);
+    descriptionHolder.appendChild(addLense);
+    descriptionHolder.appendChild(addPrice);
+  }
+}
+
+
+
+/*
 const getArticle = fetch("http://localhost:3000/api/cameras")
 .then(response => response.json())
 .then(response => alert(JSON.stringify(response)))
@@ -30,3 +104,4 @@ fetch('https://example.com/profile', {
 .catch((error) => {
   console.error('Error:', error);
 });
+*/
