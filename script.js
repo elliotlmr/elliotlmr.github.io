f0ba1c245875eea552c5d0594fc654694b39767a
Elliot Lemaire <lemaireelliot@wanadoo.fr>
2020-10-28 11:37:24 +0100

    ajout des requetes Store.html, affichage des articles

--- script.js
+++ script.js
@@ -4,41 +4,28 @@ var itemsStorage = document.getElementById('items-storage'); //Chemin vers la li
 
 
 //Au chargement de la page Boutique.
-document.body.onload = function() {
+document.body.onload = function () {
   //Initialisation de la requete fetch.
   fetch('http://localhost:3000/api/cameras', {
     method: 'GET',
-    headers: {'content-type': 'application/json'}
-  }).then(function(response) {
+    headers: { 'content-type': 'application/json' }
+  }).then(function (response) {
     return response.json();
-  }).then(function(json) {
+  }).then(function (json) {
     let items = json;
     initialize(items);
-  }).catch(function(err) {
+  }).catch(function (err) {
+    console.log(err);
     itemsStorage.innerHTML = "Impossible d'afficher les articles.";
   });
 }
 
 //Fonction globale permettant d'afficher les articles disponibles.
-function initialize(items) {
-  itemsList = items; //Database globale des articles
-  updateItems();
-  itemsList = [];
-
-  //Fonction qui crée les chemins d'accès aux images des articles.
-  /*
-  function fetchBlob(item) {
-    let url = 'back-end/images/' + product.image;
-    fetch(url).then(function(response) {
-      return response.blob();
-    }).then(function(blob) {
-      let itemPath = URL.createObjectURL(blob);
-      updateItems(itemPath, item)
-    });
-  }*/
+function initialize(items) { //Database globale des articles
+  items.map(item => updateItems(item));
 
   //Fonction permettant d'actualiser l'affichage des articles.
-  function updateItems(itemPath, item) {
+  function updateItems(item) {
     //Création des éléments constituant un Item.
     const addItem = document.createElement('div');
     const imageHolder = document.createElement('div');
@@ -48,19 +35,25 @@ function initialize(items) {
     const addText = document.createElement('p');
     const addLense = document.createElement('h4');
     const addPrice = document.createElement('h4');
+    const addButton = document.createElement('button');
 
-    //Définition des classes pour chaque div composant un article.
+    //Définition des classes pour chaque parties composant un article.
     addItem.setAttribute('class', 'row store-item');
     imageHolder.setAttribute('class', 'col-12 col-lg-4');
     descriptionHolder.setAttribute('class', 'col-12 col-lg-8');
+    addButton.setAttribute('class', 'btn btn-dark float-right');
+    addButton.setAttribute('type', 'submit');
 
-    //Ajout du contenu des titres et descriptions des articles.
+    //Ajout du contenu, des titres et descriptions des articles.
     addName.textContent = item.name;
     addText.textContent = item.description;
+    addPrice.textContent = item.price + ' €';
+    addButton.innerHTML = 'Ajouter au panier';
 
-    //Création des attributs src et alt des images.
-    img.src = item.imageUrl;
-    img.alt = item.name;
+    //Création des attributs, src et alt des images.
+    addImage.src = item.imageUrl;
+    addImage.alt = item.name;
+    addImage.setAttribute('width', '100%');
 
     //Répartition des différents éléments composant un article.
     itemsStorage.appendChild(addItem);
@@ -71,40 +64,6 @@ function initialize(items) {
     descriptionHolder.appendChild(addText);
     descriptionHolder.appendChild(addLense);
     descriptionHolder.appendChild(addPrice);
+    descriptionHolder.appendChild(addButton);
   }
 }
-
-
-
-/*
-const getArticle = fetch("http://localhost:3000/api/cameras")
-.then(response => response.json())
-.then(response => alert(JSON.stringify(response)))
-.catch(error => alert("Erreur : " + error));
-
-
-
-
-fetch('http://example.com/movies.json')
-  .then(response => response.json())
-  .then(data => console.log(data));
-
-// Fetch pour POST un JSON-encoded data
-
-  const data = { username: 'example' };
-
-fetch('https://example.com/profile', {
-  method: 'POST', // or 'PUT'
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify(data),
-})
-.then(response => response.json())
-.then(data => {
-  console.log('Success:', data);
-})
-.catch((error) => {
-  console.error('Error:', error);
-});
-*/
